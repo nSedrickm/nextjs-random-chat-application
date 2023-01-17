453a17aaef0a3ec7f7ed5f606104e3dbbfee4da8
nsedrickm <nsedrick101@gmail.com>
2023-01-17 18:59:11 +0100

    Updates

---
+++ _next/static/vF2GMIaV26txc_YVzz2Uv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/835-eb5ef39bd76368bc.js","static/chunks/pages/index-b2e0cba95e851415.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
